b4738339581d8762daeb4ec82832d359d0e05b39
ChandlerGuan <bonboru.guan@gmail.com>
2017-12-12 20:26:08 +0900

    initializaiton

---
+++ generate_list_file.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 20:23:12 2017
+
+@author: chandler
+"""
+#import os
+#
+#def geneListfile():
+#	trainPath = "train";
+#	listFile = open('trainFilelist.txt', 'w');
+#	for parent,dirnames,filenames in os.walk(trainPath): 
+#		dirnames.sort();
+#		for dirname in dirnames:
+#			for subParent,subDirnames,subFilenames in os.walk(os.path.join(trainPath,dirname)):
+#				subFilenames.sort();            
+#				for filename in subFilenames:
+#					listFile.write(os.path.join(dirname,filename)+' '+dirname[1]+'\n');	            
+#	listFile.close();
+#    
+#if __name__ == "__main__":
+#    geneListfile();
