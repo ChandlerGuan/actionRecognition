a41845520dd3254a751d7411e011cdf77f5ecc80
ChandlerGuan <bonboru.guan@gmail.com>
2017-12-13 16:40:47 +0900

    composiziton of scripts

--- geneTestDB.sh
+++ geneTestDB.sh
@@ -1,2 +1,3 @@
 rm -rf ~/work/git/caffe/data/actionTest
-build/tools/convert_imageset --shuffle --resize_height=256 --resize_width=256 ~/work/src/action/dataset/ ~/work/src/action/actionTest.txt  ~/work/git/caffe/data/actionTest
+/home/tracking/work/git/caffe/build/tools/convert_imageset --shuffle --resize_height=256 --resize_width=256 ~/work/src/action/dataset/ ~/work/src/action/actionTest.txt  ~/work/git/caffe/data/actionTest
+/home/tracking/work/git/caffe/build/tools/compute_image_mean /home/tracking/work/git/caffe/data/actionTest /home/tracking/work/git/caffe/data/actionTest/testMean.binaryproto
--- geneTrainDB.sh
+++ geneTrainDB.sh
@@ -1,2 +1,3 @@
 rm -rf ~/work/git/caffe/data/actionTrain
 build/tools/convert_imageset --shuffle --resize_height=256 --resize_width=256 ~/work/src/action/dataset/ ~/work/src/action/actionTrain.txt  ~/work/git/caffe/data/actionTrain
+/home/tracking/work/git/caffe/build/tools/compute_image_mean /home/tracking/work/git/caffe/data/actionTrain /home/tracking/work/git/caffe/data/actionTrain/trainMean.binaryproto
